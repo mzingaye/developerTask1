b06ef172af9431c9c13cdffc53f38c9c55e86a63
Mzingaye Masilela <lyne.k.m@gmail.com>
2018-05-06 13:42:30 +0200

    Issue 13 ----------------------------------------------------------------------- [Electronic-Payments-API] - com.econetwireless.epay.api.rest.resources.EpayResource ----------------------------------------------------------------------- > Line 33 - Added @PathVariable annotation with the value partnerCode

--- electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
+++ electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
@@ -6,7 +6,8 @@ import com.econetwireless.epay.api.rest.messages.TransactionsResponse;
 import com.econetwireless.utils.messages.AirtimeBalanceResponse;
 import com.econetwireless.utils.messages.AirtimeTopupRequest;
 import com.econetwireless.utils.messages.AirtimeTopupResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +23,14 @@ public class EpayResource {
 
     private ReportingProcessor reportingProcessor;
 
+    public EpayResource(EpayRequestProcessor epayRequestProcessor, ReportingProcessor reportingProcessor) {
+        this.epayRequestProcessor = epayRequestProcessor;
+        this.reportingProcessor = reportingProcessor;
+    }
+
     @GetMapping(value = "enquiries/{partnerCode}/balances/{mobileNumber}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public AirtimeBalanceResponse enquireAirtimeBalance( final String pCode, @PathVariable("mobileNumber") final String msisdn) {
+    public AirtimeBalanceResponse enquireAirtimeBalance(@PathVariable("partnerCode") final String pCode, @PathVariable("mobileNumber") final String msisdn) {
         return epayRequestProcessor.enquireAirtimeBalance(pCode, msisdn);
     }
 
